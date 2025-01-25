2df8bd698283eacd6f99644d0998fd678764c4fc
Guilherme Inoe Araujo <guilhermeinoe82@gmail.com>
2025-01-25 15:10:56 -0300

    feat: :sparkles: melhorias visuais e de usabilidade

--- src/app/aluno/page.tsx
+++ src/app/aluno/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid';
 import Link from "next/link";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:5000';
@@ -24,6 +24,7 @@ export default function AlunoDashboard() {
   const [vagas, setVagas] = useState<Vaga[]>([]);
   const [myVagas, setMyVagas] = useState<Vaga[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -134,31 +135,33 @@ export default function AlunoDashboard() {
     setCurrentPage(pageNumber);
   };
 
+  const toggleAccordion = () => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
+
   const paginatedVagas = vagas
     .filter((vaga) => !myVagas.some((myVaga) => myVaga.vaga_id === vaga.vaga_id))
     .slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
 
   const generatePaginationButtons = () => {
-    const visiblePages = 5; // Número máximo de páginas a serem exibidas
+    const visiblePages = 5;
     let pages = [];
 
     if (totalPages <= visiblePages) {
-      // Exibe todas as páginas se totalPages for menor ou igual ao limite
       pages = Array.from({ length: totalPages }, (_, i) => i + 1);
     } else {
-      // Caso contrário, gera páginas dinamicamente
       const startPage = Math.max(1, currentPage - 2);
       const endPage = Math.min(totalPages, currentPage + 2);
 
-      if (startPage > 1) pages.push(1); // Adiciona o botão para a primeira página
-      if (startPage > 2) pages.push(-1); // Reticências
+      if (startPage > 1) pages.push(1);
+      if (startPage > 2) pages.push(-1);
 
       for (let i = startPage; i <= endPage; i++) {
         pages.push(i);
       }
 
-      if (endPage < totalPages - 1) pages.push(-1); // Reticências
-      if (endPage < totalPages) pages.push(totalPages); // Adiciona o botão para a última página
+      if (endPage < totalPages - 1) pages.push(-1);
+      if (endPage < totalPages) pages.push(totalPages);
     }
 
     return pages.map((page, index) =>
@@ -183,107 +186,97 @@ export default function AlunoDashboard() {
   };
 
   return (
-    <div>
-        <header className="w-full bg-white text-stone-600 p-4">
-            <div className="max-w-7xl mx-auto flex items-center justify-between">
-            <div className="flex items-center space-x-4">
-                <img src="/images/Logo_PI.png" alt="Logo" className="h-20" width={80} height={80} />
-                <h1 className="text-2xl font-semibold">Portal Pesquisa e Extensão</h1>
-            </div>
-            <nav className="space-x-4 flex items-center">
-                <Link href="/" className="hover:text-gray-300">Home</Link>
-                <Link href="/sobre" className="hover:text-gray-300">Sobre</Link>
-                <Link href="/contato" className="hover:text-gray-300">Contato</Link>
-                
-                <button
-                    onClick={handleLogout}
-                    className="text-sm font-semibold text-slate-200 bg-red-600 hover:bg-red-700 rounded-md px-4 py-2"
-                    >
-                    Logout
-                </button>
-            </nav>
-            </div>
-        </header>
+    <div className="min-h-screen flex flex-col">
+      <header className="w-full bg-white text-stone-600 p-4">
+        <div className="max-w-7xl mx-auto flex items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <img src="/images/Logo_PI.png" alt="Logo" className="h-20" width={80} height={80} />
+            <h1 className="text-2xl font-semibold">Portal Pesquisa e Extensão</h1>
+          </div>
+          <nav className="space-x-4 flex items-center">
+            <Link href="/" className="hover:text-gray-300">Home</Link>
+            <Link href="/sobre" className="hover:text-gray-300">Sobre</Link>
+            <Link href="/contato" className="hover:text-gray-300">Contato</Link>
+            <button
+              onClick={handleLogout}
+              className="text-sm font-semibold text-slate-200 bg-red-600 hover:bg-red-700 rounded-md px-4 py-2"
+            >
+              Logout
+            </button>
+          </nav>
+        </div>
+      </header>
 
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-        <div className="relative w-1/2">
-            <div className="bg-white p-6 rounded-md shadow-md">
-            <h2 className="text-2xl font-bold mb-4">Dashboard do Aluno</h2>
+      <main className="flex-grow flex justify-center items-center bg-gray-100">
+        <div className="relative w-full max-w-4xl p-6 bg-white rounded-md shadow-md">
+          <h2 className="text-2xl font-bold mb-4">Dashboard do Aluno</h2>
 
-            <h3 className="text-xl font-semibold mb-4">Vagas Inscritas</h3>
-            <ul className="mb-4">
-                {myVagas.map((vaga) => (
-                <li key={vaga.vaga_id} className="border-b py-2">
-                    <div className="text-xl font-semibold">Nome: {vaga.nome}</div>
-                    <div className="text-gray-700">Descrição: {vaga.descricao}</div>
-                    <div className="text-gray-700">bolsa: {vaga.bolsa}</div>
-                    <div className="text-gray-700">Valor da bolsa: {vaga.bolsa_valor}</div>
-                    <div className="text-gray-700">tipo: {vaga.tipo}</div>
-                    <div className="text-gray-700">Professor: {vaga.criador_id}</div>
-                    <div className="text-gray-700">Inscritos: {vaga.inscritos}</div>
-                    <button
+          <div className="border-b">
+            <button
+              onClick={toggleAccordion}
+              className="flex justify-between items-center w-full py-2 text-lg font-semibold text-gray-700"
+            >
+              Vagas Inscritas ({myVagas.length} {myVagas.length === 1 ? 'vaga inscrita' : 'vagas inscritas'})
+              {isAccordionOpen ? (
+                <ChevronUpIcon className="h-5 w-5" />
+              ) : (
+                <ChevronDownIcon className="h-5 w-5" />
+              )}
+            </button>
+            <div
+              className={`overflow-hidden transition-all duration-300 ease-in-out`}
+              style={{
+                maxHeight: isAccordionOpen ? '500px' : '0', // Define a max-height fixa para animação
+              }}
+            >
+              {myVagas.map((vaga) => (
+                <div key={vaga.vaga_id} className="p-4 border rounded-md mb-2 bg-gray-50">
+                  <p className="text-gray-700"><strong>Vaga:</strong> {vaga.nome}</p>
+                  <p className="text-gray-700"><strong>Descrição:</strong> {vaga.descricao}</p>
+                  <button
                     onClick={() => handleDesinscreverVaga(vaga.vaga_id)}
-                    className="bg-red-500 text-white p-1 rounded-md hover:bg-red-600 mt-2"
-                    >
+                    className="bg-red-500 text-white p-2 rounded-md hover:bg-red-600 mt-2"
+                  >
                     Desinscrever-se
-                    </button>
-                </li>
-                ))}
-            </ul>
-
-            <h3 className="text-xl font-semibold mb-4">Vagas Disponíveis</h3>
-            <div className="max-h-96 overflow-y-auto mb-4">
-                <ul>
-                {paginatedVagas.map((vaga) => (
-                    <li key={vaga.vaga_id} className="border-b py-2">
-                        <div className="text-xl font-semibold">Nome: {vaga.nome}</div>
-                        <div className="text-gray-700">Descrição: {vaga.descricao}</div>
-                        <div className="text-gray-700">bolsa: {vaga.bolsa}</div>
-                        <div className="text-gray-700">Valor da bolsa: {vaga.bolsa_valor}</div>
-                        <div className="text-gray-700">tipo: {vaga.tipo}</div>
-                        <div className="text-gray-700">Professor: {vaga.criador_id}</div>
-                        <div className="text-gray-700">Inscritos: {vaga.inscritos}</div>
-                    <button
-                        onClick={() => handleInscreverVaga(vaga.vaga_id)}
-                        className="bg-green-500 text-white p-1 rounded-md hover:bg-green-600 mt-2"
-                    >
-                        Inscrever-se
-                    </button>
-                    </li>
-                ))}
-                </ul>
+                  </button>
+                </div>
+              ))}
             </div>
+          </div>
 
-            <div className="flex items-center justify-center space-x-2 mt-4">
-                <button
-                onClick={handlePrevious}
-                disabled={currentPage === 1}
-                className={`px-4 py-2 rounded-l-md ${
-                    currentPage === 1
-                    ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
-                    : 'bg-white text-gray-700 hover:bg-gray-50'
-                }`}
-                >
-                <ChevronLeftIcon className="h-5 w-5" />
-                </button>
-
-                {generatePaginationButtons()}
-
-                <button
-                onClick={handleNext}
-                disabled={currentPage === totalPages}
-                className={`px-4 py-2 rounded-r-md ${
-                    currentPage === totalPages
-                    ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
-                    : 'bg-white text-gray-700 hover:bg-gray-50'
-                }`}
-                >
-                <ChevronRightIcon className="h-5 w-5" />
-                </button>
+          <h3 className="text-xl font-semibold mb-4 mt-6">Vagas Disponíveis</h3>
+          {paginatedVagas.map((vaga) => (
+            <div key={vaga.vaga_id} className="p-4 border rounded-md mb-2">
+              <p className="text-gray-700"><strong>Vaga:</strong> {vaga.nome}</p>
+              <p className="text-gray-700"><strong>Descrição:</strong> {vaga.descricao}</p>
+              <button
+                onClick={() => handleInscreverVaga(vaga.vaga_id)}
+                className="bg-green-500 text-white p-2 rounded-md hover:bg-green-600 mt-2"
+              >
+                Inscrever-se
+              </button>
             </div>
-            </div>
+          ))}
+
+          <div className="flex justify-between items-center mt-4">
+            <button
+              onClick={handlePrevious}
+              className={`px-4 py-2 rounded-md ${currentPage === 1 ? 'bg-gray-200' : 'bg-gray-500 text-white hover:bg-gray-600'}`}
+              disabled={currentPage === 1}
+            >
+              <ChevronLeftIcon className="h-5 w-5" />
+            </button>
+            <div className="flex space-x-2">{generatePaginationButtons()}</div>
+            <button
+              onClick={handleNext}
+              className={`px-4 py-2 rounded-md ${currentPage === totalPages ? 'bg-gray-200' : 'bg-gray-500 text-white hover:bg-gray-600'}`}
+              disabled={currentPage === totalPages}
+            >
+              <ChevronRightIcon className="h-5 w-5" />
+            </button>
+          </div>
         </div>
-        </div>
+      </main>
     </div>
   );
 }
